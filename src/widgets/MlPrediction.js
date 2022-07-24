bb83dff94a62d64dafa04535d0aff81e98b66167
maamalama <bokarevstefan475@gmail.com>
2022-07-24 02:49:54 -0500

    new step

--- src/widgets/MlPrediction.js
+++ src/widgets/MlPrediction.js
@@ -159,8 +159,8 @@ const MlPrediction = () => {
         <Slider
           value={priceSliderValue}
           min={0}
-          step={0.1}
-          max={10}
+          step={0.01}
+          max={1}
           onChange={handlePriceChange}
           valueLabelDisplay="off"
           marks
